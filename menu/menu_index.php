792d632b336eb8f4f5f49c06faf40ddb13f5702f
Alfredo Arámburo <panchopistolas159753@gmail.com>
2023-05-01 01:14:32 -0600

    ADD: pagina inicial y directoriso auxiliares

---
+++ menu/menu_index.php
@@ -0,0 +1,24 @@
+<!--HEADER-->
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">ServiCars</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="index.php">Inicio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="login.php">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="registrarse.php">Registrarse</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!--/HEADER-->
---
+++ registrarse.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/bootstrap.css" media="screen" rel="stylesheet">
+    <title>Registrarse | </title>
+</head>
+<body>
+<?php
+    include 'menu/menu_index.php';
+    ?>
+</body>
+</html>
