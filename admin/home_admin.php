5c5e003c67a2692ac6f18746e5531240da458ac5
Alfredo Arámburo <panchopistolas159753@gmail.com>
2023-05-01 02:33:30 -0600

    ADD: menu admin

---
+++ admin/home_admin.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../css/bootstrap.css" media="screen" rel="stylesheet">
+    <title>Inicio | </title>
+</head>
+<body>
+    <?php
+    include "../menu/menu_admin.php";
+    ?>
+</body>
+</html>
---
+++ menu/menu_admin.php
@@ -0,0 +1,31 @@
+<!--HEADER-->
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">ServiCars</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="#">Citas</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Reportes</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Usuarios
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <a class="dropdown-item" href="#">Mecánicos</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Clientes</a>
+                    </div>
+                </li>
+                
+            </ul>
+        </div>
+    </div>
+</nav>
+<!--/HEADER-->
