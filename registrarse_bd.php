efae5ce52dd81e5194bc7cfada7d8345a3166388
Alfredo Arámburo <panchopistolas159753@gmail.com>
2023-05-02 23:23:52 -0600

    FIX: consulta login

--- registrarse_bd.php
+++ registrarse_bd.php
@@ -7,7 +7,7 @@ if ($_POST['captcha'] != $_SESSION['captcha']) {
     header("Location: registrarse.php?e=1");
 }
 
-$cad = "insert into usuario_cat set nombre='" . $_POST['nombre'] . "', primer_ap='" . $_POST['primerAp'] . "', segundo_ap='" . $_POST['segundoAp'] . "', correo='" . $_POST['email'] . "', contrasena=password('" . $_POST['pass'] . "')";
+$cad = "insert into usuario_cat set nombre='" . $_POST['nombre'] . "', primer_ap='" . $_POST['primerAp'] . "', segundo_ap='" . $_POST['segundoAp'] . "', correo='" . $_POST['email'] . "', contrasena=MD5('" . $_POST['pass'] . "')";
 
 // if (!$mail->Send()) {
 //     echo  "Error: " . $mail->ErrorInfo;
--- validar.php
+++ validar.php
@@ -19,30 +19,32 @@ session_start();
 
 include "class/classBD.php";
 
-if(isset($_GET['email']) && isset($_GET['password'])){
-    if($_GET['email']>'' && $_GET['password']>''){
+if (isset($_GET['email']) && isset($_GET['password'])) {
+    if ($_GET['email'] > '' && $_GET['password'] > '') {
 
-        $query="Select * from usuario_cat where correo='".$_GET['email']."' and contrasena=password('".$_GET['password']."')";
+        $query = "SELECT * FROM usuario_cat WHERE correo='" . $_GET['email'] . "' and contrasena=MD5('" . $_GET['password'] . "')";
 
-        $registro=$objeBD->getTupla($query);
+        $registro = $objeBD->getTupla($query);
 
-        if($objeBD->bloqueRegistros){
-            $_SESSION['nombre']=$registro->nombre." ".$registro->primer_ap." ".$registro->segundo_ap;
-            $_SESSION['correo']=$registro->correo;
-            $_SESSION['id']=$registro->id_usuario;
-            if($registro->rol==2){
+        //echo $registro;
+
+        if ($objeBD->bloqueRegistros) {
+            $_SESSION['nombre'] = $registro->nombre . " " . $registro->primer_ap . " " . $registro->segundo_ap;
+            $_SESSION['correo'] = $registro->correo;
+            $_SESSION['id'] = $registro->id_usuario;
+
+            if ($registro->rol == 2) {
                 header("location: admin/home_admin.php");
-            }else{
+            } else {
                 //$objeBD->consulta("update usuario set fechaUltAcceso='".date("Y-m-d H:i:s")."', numeAccesos=numeAccesos+1 where id=".$registro->id);
-                //header("location: users/home.php");
-            } 
-        }else{
+                header("location: regular_user/home_users.php");
+            }
+        } else {
             header("location: login.php?e=3");
         }
-    }else{
+    } else {
         header("location: login.php?e=1");  //  no hay ninguna info en los correos 
     }
-}else{
+} else {
     header("location: login.php?e=2");  //  no jala nada de la BD
 }
-?>
