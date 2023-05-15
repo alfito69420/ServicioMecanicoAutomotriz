ed8406e4f04b7a95d223d0349c3aed4a7a7f57b8
Alfredo Arámburo <panchopistolas159753@gmail.com>
2023-05-14 23:43:47 -0600

    ADD: perfil admin

--- admin/admin_perfil.php
+++ admin/admin_perfil.php
@@ -15,6 +15,56 @@
     <?php
     include "../menu/menu_admin.php";
     ?>
+
+<div class="container-fluid">
+        <div class="row d-flex justify-content-center">
+            <div class="col-md-5 mt-5 pt-5">
+                <div class="row z-depth-3">
+                    <div class="col-sm-4 bg-info rounded-left">
+                        <div class="card-block text-center text-white">
+                            <i class="fas fa-user-tie fa-7x mt-5"></i>
+                            <h2 class="font-weight-bold mt-4">Hola</h2>
+                            <p>Hola</p>
+                            <i class="far fa-edit fa-2x mb-4"></i>
+                        </div>
+                    </div>
+                    <div class="col-sm-8 bg-secondary rounded-right">
+                        <h3 class="mt-3 text-center">Información</h3>
+                        <!-- <hr class="badge-secondary w-50"> -->
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <p class="font-weight-bold">Nombre:</p>
+                                <h6 class="text-muted">
+                                    <?php
+                                    echo $_SESSION['nombre'];
+                                    ?>
+                                </h6>
+                            </div>
+                            <div class="col-sm-6">
+                                <p class="font-weight-bold">Correo:</p>
+                                <h6 class="text-muted">el email de este cabron</h6>
+                            </div>
+                            <h4 class="mt-3">AAAAAAAAAAAAA</h4>
+                            <!-- <hr class="badge-secondary w-50"> -->
+                            <div class="row">
+                                <div class="col-sm-6">
+                                    <p class="font-weight-bold">TEXTO</p>
+                                    <h6 class="text-muted">TEXTOel email de este cabron</h6>
+                                </div>
+                                <div class="col-sm-6">
+                                    <p class="font-weight-bold">TEXTO</p>
+                                    <h6 class="text-muted">TEXTO</h6>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                    <a class="btn btn-danger col-md-12 mt-3" href="../index.php" role="button">Cerrar Sesión</a>
+                </div>
+
+            </div>
+        </div>
+    </div>
     
     <?php
     include '../footer.php';
